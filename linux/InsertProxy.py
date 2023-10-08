23c1fb21c2f8216f99ba316e9969e143811c5170
alimirmohammad <ali@13>
2023-10-08 19:57:23 -0400

    Update #2

--- linux/InsertProxy.py
+++ linux/InsertProxy.py
@@ -4,40 +4,7 @@ from extractdata import ipS,portS,typeS
 from checkdependencies import check_for_proxy_chains_installation
 
 
-
-# def insert_proxies_to_config(ipS=ipS, portS=portS, typeS=typeS):
-#     try:
-#         config_file = "/etc/proxychains4.conf"
-
-#         # Read the contents of the config file
-#         with open(config_file, "r") as file:
-#             config_lines = file.readlines()
-
-#         # Find the desired section in the config file
-#         proxy_list_start = None
-#         for i, line in enumerate(config_lines):
-#             if line.startswith("[ProxyList]"):
-#                 proxy_list_start = i + 1
-#                 break
-
-#         if proxy_list_start is not None:
-#             # Construct new proxy lines from the provided lists
-#             new_proxy_lines = [f"{proxy_type} {ip} {port}\n" for ip, port, proxy_type in zip(ipS, portS, typeS)]
-
-#             # Insert the new proxy lines into the config file
-#             config_lines[proxy_list_start:proxy_list_start] = new_proxy_lines
-
-#             # Write the updated contents back to the config file
-#             with open(config_file, "w") as file:
-#                 file.writelines(config_lines)
-#     except Exception as e:
-#         print(e)
-
-# # Filled lists of proxy information
-# # Call the function
-# insert_proxies_to_config()
-
-def insert_proxies_to_config(ipS=ipS, portS=portS, typeS=typeS):
+async def insert_proxies_to_config(ipS=ipS, portS=portS, typeS=typeS):
     try:
         config_file = "/etc/proxychains4.conf"
 
@@ -82,4 +49,4 @@ def insert_proxies_to_config(ipS=ipS, portS=portS, typeS=typeS):
 # Call the function
 insert_proxies_to_config(ipS, portS, typeS)
 
-print(ipS)
+# print(I)
--- linux/main.py
+++ linux/main.py
@@ -1,4 +1,8 @@
-
+import asyncio
+import InsertProxy
+from extractdata import extract_data_from_database
+import threading
+from checkdependencies import check_for_proxy_chains_installation,check_for_system_tor_installation,install_system_tor
 
 logo = """                                                                          
                                                                                                                                   
@@ -10,5 +14,35 @@ logo = """
                                                    
 
                                                                                                       """
+import asyncio
+import threading
 
-print(logo)
+import asyncio
+
+async def main():
+    print(logo)
+    while True:
+        start = input("Please enter Yes to start the process and Type 'exit' to exit the program.")
+        match start:
+            case "Yes":
+                tasks = [
+                    asyncio.to_thread(check_for_proxy_chains_installation),
+                    check_for_system_tor_installation()
+                ]
+
+                results = await asyncio.gather(*tasks)
+
+                if all(results):
+                    print("All dependencies are installed.")
+                    tasksNext = [
+                        extract_data_from_database(),
+                        InsertProxy.insert_proxies_to_config()
+                    ]
+                    resultNext = await asyncio.gather(*tasksNext)
+                    if all(resultNext):
+                        print("Proxies inserted successfully!")
+                        print("All th tasks has been\n done.Please enter /etc/proxychains4.conf \nfile and remove the # of the dynamic chain and add #\n for strict chain to be able to use proxy chains.")
+            case "no":
+                exit()
+
+asyncio.run(main())
