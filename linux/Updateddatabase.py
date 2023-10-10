5c5769f79d56d36df3deee65a64811c7eeb20252
alimirmohammad <ali@13>
2023-10-10 15:45:03 -0400

    Database #4

--- linux/Updateddatabase.py
+++ linux/Updateddatabase.py
@@ -13,140 +13,37 @@ class Singleton(type):
 class Database(metaclass=Singleton):
     
     def __init__(self):
-        self.db_file = "proxyservers.db"
+        self.db_file = "test3.db"
     
-    async def data_base_updated_for_proxyservers_holding(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS proxyserver (ip TEXT, port TEXT, type TEXT, col4 TEXT)")
-        values = [("ip1", "port1", "type1", "4", "col5"), ("ip2", "port2", "type2", "t", "col5")]
-        cur.executemany("INSERT INTO proxyserver VALUES (?,?,?,?,?)", values)
-
-        con.commit()
-        con.close()
+    async def create_table_for_proxy_servers(self):
+        conn = sqlite3.connect(self.db_file)
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS proxy_servers (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        ip_address TEXT,
+                        port INTEGER,
+                        type TEXT
+                        )''')
+        conn.commit()
+        conn.close()
         
-    async def Insert_new_values_into_data_base(self):
-        connection = sqlite3.connect(self.db_file)
-        cur = connection.cursor()
-        values = [("ip1", "port1", "type1", "4", "col5"), ("ip2", "port2", "type2", "t", "col5")]
-        cur.executemany("INSERT INTO proxyserver VALUES (?,?,?,?,?)", values)
-
-        connection.commit()
-        connection.close()
-
-    async def display_data_base_info(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM proxyserver")
-        rows = cur.fetchall()
-        print(rows)
-        con.close()
+    async def insert_some_value(self):
+        conn = sqlite3.connect(self.db_file)
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO proxy_servers (ip_address, port, type) VALUES ("89.207.132.170", 1080, "SOCKS5")''')
+        conn.commit()
+        conn.close()
         
-    async def delete_data_from_the_data_base(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("DELETE FROM proxyserver")
-        con.commit()
-        con.close()
-        
-    async def special_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("ALTER TABLE proxyserver ADD COLUMN special TEXT")
-        con.commit()
-        con.close()
-        
-    async def insert_special_proxy_into_data_base(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("UPDATE proxyserver SET specialproxy = ? WHERE ip = ?", ("192.168.1.1", "145.239.85.58"))
-        con.commit()
-        con.close()
-        
-    async def create_saved_proxy_table(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS savedproxy (ip TEXT, port TEXT, type TEXT)")
-        con.commit()
-        con.close()
-        
-    async def insert_saved_proxy_into_data_base(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("INSERT INTO savedproxy VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
-        con.commit()
-        con.close()
-        
-    async def create_table_for_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS fastproxy (ip TEXT, port TEXT, type TEXT)")
-        con.commit()
-        con.close()
-        
-    async def insert_to_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("INSERT INTO fastproxy VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
-        con.commit()
-        con.close()
-        
-    async def display_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM fastproxy")
-        rows = cur.fetchall()
-        print(rows)
-        con.close()
-        
-    async def died_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("DELETE FROM fastproxy")
-        con.commit()
-        con.close()
-        
-    async def suspended_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("UPDATE fastproxy SET type = ? WHERE ip = ?", ("SOCKS5", "145.239.85.58"))
-        con.commit()
-        con.close()
-        
-    async def column_for_stored_fast_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("ALTER TABLE fastproxy ADD COLUMN types TEXT")
-        con.commit()
-        con.close()
-        
-    async def create_table_for_main_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS mainproxy (ip TEXT, port TEXT, type TEXT)")
-        con.commit()
-        con.close()
-        
-    async def insert_to_main_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("INSERT INTO mainproxy VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
-        con.commit()
-        con.close()
-        
-    async def display_main_proxy_servers(self):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        cur.execute("SELECT * FROM mainproxy")
-        rows = cur.fetchall()
-        print(rows)
-        con.close()
 
 async def main():
     database = Database()
-    await database.create_table_for_main_proxy_servers()
-    await database.insert_to_main_proxy_servers()
-    await database.display_main_proxy_servers()
+    await database.create_table_for_proxy_servers()
+    await database.insert_some_value()
+    # await database.create_table_for_main_proxy_servers()
+    # await database.insert_to_main_proxy_servers()
+    # await database.display_main_proxy_servers()
+    # await database.special_proxy_server_tables()
+    # await database.special_main()
     
     # await database.data_base_updated_for_proxyservers_holding()
     # await database.Insert_new_values_into_data_base()
