38554d2d58cbb583fdaf6647f90ac5ce6a4acfe9
alimirmohammad <ali@13>
2023-10-10 10:13:20 -0400

    BACKENDUPDATE #1

--- linux/Updateddatabase.py
+++ linux/Updateddatabase.py
@@ -29,8 +29,101 @@ class Database(metaclass=Singleton):
         
     async def Insert_new_values_into_data_base():
         global ipS, portS, typeS
-        pass
+        connection = sqlite3.connect("proxyservers.db")
+        cur = connection.cursor()
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5")) #!complete
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5")) #!complete
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5")) #! complete
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5")) #!complete
+        cur.execute("INSERT INTO proxyserver VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5")) #!complete
+        connection.commit()
+        connection.close()
     #TODO: complete here
+    
+    async def display_data_base_info(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM proxyserver")
+        rows = cur.fetchall()
+        print(rows)
+        
+    async def delete_data_from_the_data_base(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("DELETE FROM proxyserver")
+        con.commit()
+        con.close()
+        
+    async def special_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("ALERT TABLE proxyserver ADD COLUMN specialproxy")
+        con.commit()
+        con.close()
+        
+    async def insert_special_proxy_into_data_base(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("UPDATE proxyserver SET specialproxy = ? WHERE condition",("192.168.1.1","8080","http"))
+        con.commit()
+        con.close()
+        
+    async def create_saved_proxy_table(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS savedproxy (ip TEXT, port TEXT, type TEXT)")
+        con.commit()
+        con.close()
+        
+    async def insert_saved_proxy_into_data_base(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO savedproxy VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
+        con.commit()
+        con.close()
+        
+    async def create_table_for_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS fastproxy (ip TEXT, port TEXT, type TEXT)")
+        con.commit()
+        con.close()
+        
+    async def insert_to_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO fastproxy VALUES (?,?,?)",("145.239.85.58","9300","SOCKS5"))
+        con.commit()
+        con.close()
+        
+    async def display_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM fastproxy")
+        rows = cur.fetchall()
+        print(rows)
+        
+    async def died_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("DELETE FROM fastproxy")
+        con.commit()
+        con.close()
+        
+    async def suspended_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("UPDATE fastproxy SET type = ? WHERE condition",("SOCKS5","http"))
+        con.commit()
+        con.close()
+        
+    async def column_for_stored_fast_proxy_servers(self):
+        con = sqlite3.connect("proxyservers.db")
+        cur = con.cursor()
+        cur.execute("ALTER TABLE fastproxy ADD COLUMN type TEXT")
+        con.commit()
+        con.close()
     
     #? can use assert 
 
